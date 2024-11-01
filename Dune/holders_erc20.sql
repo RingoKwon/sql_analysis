71d54fed8122bbe5097142c4dc4302f2c051aff9
ringo <60028639+RingoKwon@users.noreply.github.com>
2024-11-01 13:39:50 +0900

    by CA

--- Dune/holders_erc20.sql
+++ Dune/holders_erc20.sql
@@ -40,59 +40,8 @@ WITH token_balances_usdc AS (
     1 = 1 AND a.contract_address = FROM_HEX('A0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48')
   GROUP BY
     2
-), token_balances_usdt AS (
-  SELECT
-    -SUM(TRY_CAST(value AS DOUBLE) / POWER(10, b.decimals)) AS amount,
-    "from" AS address,
-    'USDT' AS symbol,
-    'Stablecoin' AS token_type
-  FROM erc20_ethereum.evt_Transfer AS a
-  JOIN tokens.erc20 AS b
-    ON a.contract_address = b.contract_address
-  WHERE
-    1 = 1 AND a.contract_address = FROM_HEX('dAC17F958D2ee523a2206206994597C13D831ec7')
-  GROUP BY
-    2
- UNION ALL
-  SELECT
-    SUM(TRY_CAST(value AS DOUBLE) / POWER(10, b.decimals)) AS amount,
-    a.to AS address,
-    'USDT' AS symbol,
-    'Stablecoin' AS token_type
-  FROM erc20_ethereum.evt_Transfer AS a
-  JOIN tokens.erc20 AS b
-    ON a.contract_address = b.contract_address
-  WHERE
-    1 = 1 AND a.contract_address = FROM_HEX('dAC17F958D2ee523a2206206994597C13D831ec7')
-  GROUP BY
-    2
-), token_balances_dai AS (
-  SELECT
-    -SUM(TRY_CAST(value AS DOUBLE) / POWER(10, b.decimals)) AS amount,
-    "from" AS address,
-    'DAI' AS symbol,
-    'Stablecoin' AS token_type
-  FROM erc20_ethereum.evt_Transfer AS a
-  JOIN tokens.erc20 AS b
-    ON a.contract_address = b.contract_address
-  WHERE
-    1 = 1 AND a.contract_address = FROM_HEX('6B175474E89094C44Da98b954EedeAC495271d0F')
-  GROUP BY
-    2
-UNION ALL
-  SELECT
-    SUM(TRY_CAST(value AS DOUBLE) / POWER(10, b.decimals)) AS amount,
-    a.to AS address,
-    'DAI' AS symbol,
-    'Stablecoin' AS token_type
-  FROM erc20_ethereum.evt_Transfer AS a
-  JOIN tokens.erc20 AS b
-    ON a.contract_address = b.contract_address
-  WHERE
-    1 = 1 AND a.contract_address = FROM_HEX('6B175474E89094C44Da98b954EedeAC495271d0F')
-  GROUP BY
-    2
-), token_holders AS (
+)
+, token_holders AS (
   SELECT
     address,
     symbol,
